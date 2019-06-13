e6a635453e83e7808b17f26270dc88d42ae825b2
Hemant Kumar <Hemant.Kumar@accolite.com>
2019-06-13 22:38:28 +0530

    Constructor injection

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -7,6 +7,10 @@ import com.pluralsight.repository.CustomerRepository;
 
 public class CustomerServiceImpl implements CustomerService {
 	private CustomerRepository customerRepository;
+	
+	public CustomerServiceImpl(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
 
 	@Override
 	public List<Customer> findAll() {
