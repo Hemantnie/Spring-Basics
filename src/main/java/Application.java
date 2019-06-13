1bc3dcb00a82083769cf985696edd0405d58943d
Hemant Kumar <Hemant.Kumar@accolite.com>
2019-06-13 21:50:12 +0530

    Initial setup for application

---
+++ src/main/java/Application.java
@@ -0,0 +1,13 @@
+import com.pluralsight.service.CustomerService;
+import com.pluralsight.service.CustomerServiceImpl;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final CustomerService customerService = new CustomerServiceImpl();
+		System.out.println(customerService.findAll().get(0).getFirstName());
+
+	}
+
+}
