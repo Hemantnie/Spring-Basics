65993e605952492f4dc301592cc50697bf182d5c
Hemant Kumar <Hemant.Kumar@accolite.com>
2019-06-14 00:07:26 +0530

    Autowire

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,15 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.pluralsight.service.CustomerService;
-import com.pluralsight.service.CustomerServiceImpl;
 
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		final CustomerService customerService = new CustomerServiceImpl();
+
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		CustomerService customerService = appContext.getBean("customerService", CustomerService.class);
 		System.out.println(customerService.findAll().get(0).getFirstName());
 
 	}
